939b5160a536bd88ae5b076e5064865cab9ef137
RasmusMS <33825914+RasmusMS@users.noreply.github.com>
2019-11-08 10:25:53 +0100

    Added login validation with hashed passwords

--- class/DbCon.php
+++ class/DbCon.php
@@ -7,29 +7,39 @@ class DbCon {
   private $user = 'root';
   private $pw = 'root';
   private $db = 'ssp';
-  private $con;
+  public $con;
 
   function __construct(){
-    // Instantier MYSQLI objekt til datahåndtering
+    // Creates an instance of the MYSQLI object for datahandling
     $this->con = new mysqli($this->host, $this->user, $this->pw, $this->db);
 
-    // Tester om forbindelsen virker, hvis ikke så lukker det forbindelsen
+    // Testing if the connection works. If not then shuts it down
     if($this->con->connect_errno != ''){
       echo '<h1>'.$this->con->connect_error.'</h1>';
       die('Der er ikke forbindelse til databasen');
     } else {
-      // Sætter charset
+      // Setting the charset to UTF8
       $this->con->set_charset('UTF8');
     }
   }
 
-  // Hente alle produkter pg returnere et array af produkt objekter
+  public function checkSecureCon() {
+    if(!empty($_SERVER["https"])) {
+      echo "Connection secure!";
+    } else {
+      die("Connection not secure!");
+    }
+  }
+
+  // Fetching all products and makes an array of products
   public function getAllProducts() {
+
     // Creates a new array to contain each product
     $products = array();
 
     // The SQL statement to get each product
     $sql = 'SELECT products.idProduct, products.name, products.description, products.price FROM products';
+
 
     // Queries the statement
     $result = $this->con->query($sql);
@@ -48,11 +58,16 @@ class DbCon {
     $product = null;
 
     // The SQL statement to get each product
-    $sql = "SELECT products.idProduct, products.name, products.description, products.price FROM products WHERE products.idProduct = $id";
+    $sql = $this->con->prepare("SELECT products.idProduct, products.name, products.description, products.price FROM products WHERE products.idProduct = ?");
+    $sql->bind_param("i", $thisId);
 
-    // Queries the statement
-    $result = $this->con->query($sql);
+    $thisId = $id;
 
+    // Executes the sql statement and returns true/falls
+    $sql->execute();
+    // Because it only returns true/false, I have to add this extra code to get the content of the query.
+    $result = $sql->get_result();
+    $sql->close();
     // Puts each product into the products array
     while($row = $result->fetch_object()) {
       $product = new Product($row->idProduct, $row->name, $row->description, $row->price);
@@ -62,62 +77,127 @@ class DbCon {
     return $product;
   }
 
-  /*public function getProductByName($name) {
+  public function getProductByName($name) {
+
     $product = null;
+
     $sql = $this->con->prepare("SELECT products.idProduct, products.name, products.description, products.price FROM products WHERE products.name = ?");
-    $sql->bind_param("s", $name);
-    $result = $sql->execute();
+    $sql->bind_param("s", $thisName);
+
+    $thisName = $name;
+
+    $sql->execute();
+
+    $result = $sql->get_result();
+    $sql->close();
 
     while($row = $result->fetch_object()) {
       $product = new Product($row->idProduct, $row->name, $row->description, $row->price);
     }
 
     return $product;
-  }*/
+  }
 
+  // Function for creating products using POST
   public function createProduct($name, $desc, $price) {
+
+    // Here I make a prepared statement and bind it to some parameters. This is to prevent SQL injection
     $sql = $this->con->prepare("INSERT INTO products (products.name, products.description, products.price) VALUES (?, ?, ?)");
+    // ssi stands for string, string, int. It defines what input it should expect in each parameter
     $sql->bind_param("ssi", $thisName, $thisDesc, $thisPrice);
 
+    // Here I define my parameters with the variables I get from where the function is called
     $thisName = $name;
     $thisDesc = $desc;
     $thisPrice = $price;
 
+    // Lastly I execute my sql statement in an if else statement.
     if($sql->execute()) {
       echo "Successfully created the product!";
+      $sql->close();
     } else {
+      $sql->close();
       http_response_code(400);
       die('400 - Bad Request');
     }
   }
 
-  public function updateProduct($product) {
+  // This function is used to update the data of a product using PUT
+  public function updateProduct($thisProduct) {
+    // Preparing my statement
     $sql = $this->con->prepare("UPDATE products SET name = ?, description = ?, price = ? WHERE idProduct = ?;");
     $sql->bind_param("ssii", $name, $description, $price, $id);
 
-    $name = $product->name;
-    $description = $product->description;
-    $price = $product->price;
-    $id = $product->id;
+    // defining my parameters
+    $name = $thisProduct->name;
+    $description = $thisProduct->description;
+    $price = $thisProduct->price;
+    $id = $thisProduct->id;
 
+    // Executing the SQL in an if else statement
     if($sql->execute()) {
-      echo "Successfully updated the product!";
+      return $thisProduct->name;
+      $sql->close();
     } else {
+      $sql->close();
       http_response_code(400);
       die('400 - Bad Request');
     }
   }
 
+  // This function is called whenever a product is being deleted
   public function deleteProduct($id) {
-    $sql = "DELETE FROM products WHERE idProduct = $id";
+    // Prepared statement.
+    $sql = $this->con->prepare("DELETE FROM products WHERE idProduct = ?");
+    $sql->bind_param("i", $thisId);
 
-    if($this->con->query($sql)) {
+    // Binding the id to the ID parameter
+    $thisId = $id;
+
+    // Executing the SQL
+    if($sql->execute()) {
       echo "Successfully deleted product!";
+      $sql->close();
     } else {
+      $sql->close();
       http_response_code(400);
       die('400 - Bad Request');
     }
+  }
+
+
+  public function validateLogin($userName, $passWord) {
+    $sql = $this->con->prepare("SELECT userName, passWord FROM admin WHERE userName = ?");
+    $sql->bind_param("s", $userName);
+    $sql->execute();
+    $result = $sql->get_result();
+    $sql->close();
+    $rowcount = $result->num_rows;
+
+    if($rowcount > 0) {
+      while($row = $result->fetch_assoc()) {
+        $hashed_password = $row['passWord'];
+        if(!(password_verify($passWord, $hashed_password))){
+          die("Brugernavnet og/eller adgangskoden er forkert.");
+        }
+      }
+    } else {
+      die("User doesn't exist");
+    }
+  }
+
+  public function createAdmin($userName, $passWord) {
+    $hashed_pass = password_hash("$passWord", PASSWORD_DEFAULT);
+    $sql = $this->con->prepare("INSERT INTO admin (userName, passWord) VALUES ( ?, ?)");
+    $sql->bind_param("ss", $userName, $hashed_pass);
+    $sql->execute();
+    $sql->close();
+    echo "Admin created";
+  }
+
+  function __destruct(){
+    $this->con->close();
   }
 }
 
-?>
+  ?>
--- index.php
+++ index.php
@@ -1,26 +1,24 @@
-<?php
-  /*require_once "includes/header.php";
-
-  if(file_exists("pages/$page")){
-     include_once "pages/$page";
- }
- else {
-     include_once 'pages/404.php';
- }*/
- ?>
-
  <?php
  error_reporting(E_ALL);
  ini_set('display_errors', 1);
 
  include_once 'class/DbCon.php';
 
+ // Caching not Allow
+ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+ header("Cache-Control: post-check=0, pre-check=0", false);
+ header("Pragma: no-cache");
+
  // Returns the method used. IE GET, POST, PUT or DELETE
  $httpMethod =  $_SERVER['REQUEST_METHOD'];
 
+ // Fetching the URI
  $uri = $_SERVER['REQUEST_URI'];
 
+ // Splitting it into an array whenever it meets a /
  $tempUri = explode('/', $uri);
+
+ // Removes every value in the array before it meets serversideprogrammering. Also deletes that.
  foreach($tempUri AS $key => $val) {
    unset($tempUri[$key]);
    if($val === 'serversideprogrammering') {
@@ -30,27 +28,22 @@
 
  $uri = array_values($tempUri);
 
- //echo '<pre>';
- //var_dump($uri);
- //echo '</pre>';
-
  // Request the headers
  $headers = apache_request_headers();
  $accept = str_replace(' ', '',$headers['Accept']);
  $accept = explode(',',$accept);
-
- //echo '<pre>';
- //var_dump($accept);
- //echo '</pre>';
+ $userN = $headers['User'];
+ $passW = $headers['Pass'];
 
  // Execute code based on method
  switch ($httpMethod) {
    case 'GET':
+
    // Looking at the URI for which resources we're asking for
      if($uri[0] === 'products') { // If its products it'll run this following code
        // Creates an instance of the DbCon class
        $dbCon = new DbCon();
-
+       $dbCon->validateLogin($userN, $passW);
        // Get the return value from the getAllProducts function
        $products = $dbCon->getAllProducts();
 
@@ -61,7 +54,7 @@
            echo json_encode($products);
        } else {
          http_response_code(412);
-         die('412 - Wrong accept type. Only JSON and XML supported!');
+         die('412 - Wrong accept type. Only JSON supported!');
        }
      } else if ($uri[0] === 'product' && $uri[1] === 'id' && !empty($uri[2])) {
        // Creates an instance of the DbCon class
@@ -69,17 +62,18 @@
 
        // Get the return value from the getAllProducts function
        $product = $dbCon->getProductById($uri[2]);
-
+       // Checks if the accept type is json. If it is it'll encode the product as JSON.
        if(in_array('application/json',$accept)){
          header('Access_Control-Allow-Origin: *');
          header("Content-Type: application/json; charset=utf-8");
          echo json_encode($product);
        } else {
          http_response_code(412);
-         die('412 - Wrong accept type. Only JSON and XML supported!');
+         die('412 - Wrong accept type. Only JSON supported!');
        }
      } else if ($uri[0] === 'product' && $uri[1] === 'name' && !empty($uri[2])) {
        $dbCon = new DbCon();
+       $dbCon->checkSecureCon();
 
        $product = $dbCon->getProductByName($uri[2]);
 
@@ -89,7 +83,7 @@
          echo json_encode($product);
        } else {
          http_response_code(412);
-         die('412 - Wrong accept type. Only JSON and XML supported!');
+         die('412 - Wrong accept type. Only JSON supported!');
        }
      } else {
        http_response_code(404);
@@ -104,9 +98,14 @@
        $price = $_POST['price'];
 
        $dbCon->createProduct($name, $desc, $price);
+       $product = $dbCon->getProductById(mysqli_insert_id($dbCon->con));
+       echo json_encode($product);
      } else if ($uri[0] === 'product'){
        http_response_code(405);
        die('405 - Method not allowed!');
+     } else if ($uri[0] === 'stocks') {
+       $dbCon = new DbCon();
+
      } else {
        http_response_code(404);
        die('404 - Page not found!');
@@ -114,14 +113,16 @@
      break;
 
    case 'PUT':
-     if($uri[0] === 'product') {
+     if($uri[0] === 'products') {
        $dbCon = new DbCon();
 
-       if($headers['Content-Type'] === 'application/json') {
+       if(in_array('application/json',$accept)) {
          $result = json_decode(file_get_contents('php://input'));
          $product = $result;
 
-         $dbCon->updateProduct($product);
+         $productName = $dbCon->updateProduct($product);
+         $thisProduct = $dbCon->getProductByName($productName);
+         echo json_encode($thisProduct);
        } else {
          echo "Something went wrong with the PUT";
        }
